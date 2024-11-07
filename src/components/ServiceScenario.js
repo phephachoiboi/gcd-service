4ed49903b4390a66662f9e042b0e3448f12c6e57
phephachoiboi <duyphephon@gmail.com>
2024-11-07 16:53:29 +0700

    section skin treatment

--- src/components/ServiceScenario.js
+++ src/components/ServiceScenario.js
@@ -2,12 +2,14 @@ import React from "react";
 import "../App.css";
 import SectionOurServices from "./SectionOurServices";
 import SectionBeautyConsultation from "./SectionBeautyConsultation";
+import SectionSkinTreatment from "./SectionSkinTreatment";
 
 const ServiceScenario = () => {
   return (
     <main className="flex flex-col m-0 p-0">
       <SectionOurServices />
       <SectionBeautyConsultation />
+      <SectionSkinTreatment />
     </main>
   );
 };
