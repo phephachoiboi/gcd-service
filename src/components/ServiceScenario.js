d988eec0369d6b6dcc1652e558852f18a16a4798
phephachoiboi <duyphephon@gmail.com>
2024-11-07 11:43:16 +0700

    header and section our services

---
+++ src/components/ServiceScenario.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../App.css";
+import SectionOurServices from "./SectionOurServices";
+
+const ServiceScenario = () => {
+  return (
+    <main className="flex flex-col m-0 p-0">
+      <SectionOurServices />
+    </main>
+  );
+};
+export default ServiceScenario;
