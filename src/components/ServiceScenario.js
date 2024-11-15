001e90175f498d7d12dbb61a1a65bb6f821a7e6f
phephachoiboi <duyphephon@gmail.com>
2024-11-15 14:02:44 +0700

    the beauty products

--- src/components/ServiceScenario.js
+++ src/components/ServiceScenario.js
@@ -3,6 +3,7 @@ import "../App.css";
 import SectionOurServices from "./SectionOurServices";
 import SectionBeautyConsultation from "./SectionBeautyConsultation";
 import SectionSkinTreatment from "./SectionSkinTreatment";
+import SectionBeautyProduct from "./SectionBeautyProduct";
 
 const ServiceScenario = () => {
   return (
@@ -10,6 +11,7 @@ const ServiceScenario = () => {
       <SectionOurServices />
       <SectionBeautyConsultation />
       <SectionSkinTreatment />
+      <SectionBeautyProduct />
     </main>
   );
 };
